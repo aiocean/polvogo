4c07da5f25187db6a9c7209d1776077a67706e73
Duoc Nguyen <nguyenvanduocit@gmail.com>
2021-05-14 15:55:51 +0700

    fix: update proto

--- aiocean/polvo/v1/polvo_service.pb.validate.go
+++ aiocean/polvo/v1/polvo_service.pb.validate.go
@@ -41,6 +41,13 @@ func (m *ListPackagesRequest) Validate() error {
 		return nil
 	}
 
+	if !_ListPackagesRequest_ApplicationOrn_Pattern.MatchString(m.GetApplicationOrn()) {
+		return ListPackagesRequestValidationError{
+			field:  "ApplicationOrn",
+			reason: "value does not match regex pattern \"(?m)^polvo.aiocean.services/applications/[^/]+$\"",
+		}
+	}
+
 	return nil
 }
 
@@ -99,6 +106,8 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = ListPackagesRequestValidationError{}
+
+var _ListPackagesRequest_ApplicationOrn_Pattern = regexp.MustCompile("(?m)^polvo.aiocean.services/applications/[^/]+$")
 
 // Validate checks the field values on ListPackagesResponse with the rules
 // defined in the proto definition for this message. If any rules are
